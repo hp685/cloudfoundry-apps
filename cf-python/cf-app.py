2ebbc0edf7b42aac5410e369477b471b7b8d0eb5
Harsh Patel <harsh@nyu.edu>
2020-02-28 10:47:44 -0800

    generic endpoint changes

--- cf-python/cf-app.py
+++ cf-python/cf-app.py
@@ -5,8 +5,17 @@ import json
 
 app = Flask(__name__)
 
+URL = "http://appd-configuration-server.apps.hiddenhills.cf-app.com/downloads/AppDynamics-DotNetCore-linux-x64-4.5.18.1.zip"
+
 @app.route('/')
 def getenv():
     print("printing appdynamics environment exposed to applicaiton")
     appd_env = {"appdynamics_env": { env: os.environ[env] for env in os.environ if env.startswith('APPD')}}
     return jsonify(appd_env)
+
+@app.route('/forward')
+def forward():
+    response = requests.get(URL, allow_redirects=True)
+    with open('agent.zip', 'wb') as f:
+        f.write(response.content)
+    return send_file('agent.zip', mimetype='application/zip', as_attachment=True, attachment_filename='agent.zip')
